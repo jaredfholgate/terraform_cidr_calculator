afaa2a5f0667099f9845415790ad555fb845848d
Jared Holgate <jaredholgate@microsoft.com>
2024-04-05 11:02:10 +0100

    Tidy

--- locals.tf
+++ locals.tf
@@ -3,7 +3,5 @@ locals {
   subnet_keys                   = keys(var.subnets_and_sizes)
   subnet_new_bits               = [for size in values(var.subnets_and_sizes) : size - var.address_space_size]
   cidr_subnets                  = cidrsubnets(local.virtual_network_address_space, local.subnet_new_bits...)
-
-  skip_nsg = ["AzureBastionSubnet", "virtual_machines"]
   subnets = { for key, value in var.subnets_and_sizes : key => local.cidr_subnets[index(local.subnet_keys, key)] }
 }
