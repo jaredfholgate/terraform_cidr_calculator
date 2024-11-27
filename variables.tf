98ee7ec451683562c4820f6a9b4ee9b936f1c495
Jared Holgate <jaredholgate@microsoft.com>
2024-11-27 11:53:10 +0000

    Test ALZ

--- variables.tf
+++ variables.tf
@@ -1,21 +1,22 @@
 variable "address_space_start_ip" {
   type        = string
   description = "The address space that is used the virtual network"
-  default     = "10.36.0.0"
+  default     = "10.0.0.0"
 }
 
 variable "address_space_size" {
   type        = number
   description = "The address space that is used the virtual network"
-  default     = 18
+  default     = 22
 }
 
 variable "subnets_and_sizes" {
   type        = map(number)
   description = "The size of the subnets"
   default = {
-    "a" = 23
-    "b" = 23
-    "c" = 23
+    "a" = 26
+    "b" = 26
+    "c" = 27
+    "d" = 28
   }
 }
