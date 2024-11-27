86ba5bd0dafc87092f47e91adec5bde988b80a4d
Jared Holgate <jaredholgate@microsoft.com>
2024-11-27 12:08:51 +0000

    tidy

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  order_by_size = { for key, value in var.subnets_and_sizes : "${format("%03s", value)}||${key}" => value }
+  order_by_size                 = { for key, value in var.subnets_and_sizes : "${format("%03s", value)}||${key}" => value }
   virtual_network_address_space = "${var.address_space_start_ip}/${var.address_space_size}"
   subnet_keys                   = keys(local.order_by_size)
   subnet_new_bits               = [for size in values(local.order_by_size) : size - var.address_space_size]
